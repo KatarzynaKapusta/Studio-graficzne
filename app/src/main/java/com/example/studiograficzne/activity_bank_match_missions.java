b3a34eb3ff40947d5159d965206ae403d64d0a57
AleksandraStopka <aleksandra.stopka15@gmail.com>
2022-04-11 21:44:02 +0200

    Podstawa lokacji bank, utworzenie oddzielnych aktywności dla misji

---
+++ app/src/main/java/com/example/studiograficzne/activity_bank_match_missions.java
@@ -0,0 +1,14 @@
+package com.example.studiograficzne;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_bank_match_missions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bank_match_missions);
+    }
+}
