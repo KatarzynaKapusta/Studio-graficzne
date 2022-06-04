fbdd69874504e53bbeb70a7a9dd5f82a2dc317c3
AleksandraStopka <aleksandra.stopka15@gmail.com>
2022-06-04 23:11:33 +0200

    Studio info - makieta początkowa

--- app/src/main/java/com/example/studiograficzne/fragment_info_improvements.java
+++ app/src/main/java/com/example/studiograficzne/fragment_info_improvements.java
@@ -2,18 +2,39 @@ package com.example.studiograficzne;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.auth.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class fragment_info_improvements extends Fragment {
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info_improvements, container, false);
+        View v =inflater.inflate(R.layout.fragment_info_improvements, container, false);
+
+
+
+        return v;
     }
+
+
 }
