2f6019f20688dd50f711f3d0d992dbf6fdf4a3d3
AleksandraStopka <aleksandra.stopka15@gmail.com>
2022-06-05 21:39:33 +0200

    Skończona apka :)

--- app/src/main/java/com/example/studiograficzne/activity_endOfGame.java
+++ app/src/main/java/com/example/studiograficzne/activity_endOfGame.java
@@ -6,9 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseUser;
+
 public class activity_endOfGame extends AppCompatActivity {
 
     private Button backToGameBtn;
+    private Button showStatsBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +24,19 @@ public class activity_endOfGame extends AppCompatActivity {
             finish();
         });
 
+        showStatsBtn = findViewById(R.id.showStatisticsButton);
+        showStatsBtn.setOnClickListener(v -> {
+            openActivityStudioStatistics();
+            finish();
+        });
+
     }
+
+    private void openActivityStudioStatistics() {
+        Intent intent = new Intent(this, activity_studio_panel_info.class);
+        startActivity(intent);
+    }
+
 
     @Override
     public void onBackPressed () {}
